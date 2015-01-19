10c2611cd5ff919f8c0eb7b37b9e4382b42ee704
Peter Organisciak <organisciak@gmail.com>
2015-01-19 14:35:28 -0600

    Rework textstream to take list of files

--- scripts/htrc_feature_textstream.py
+++ scripts/htrc_feature_textstream.py
@@ -6,15 +6,16 @@ This makes >1 grams meaningless in Bookworm, but still a good indicator of size 
 from htrc_features import FeatureReader
 import csv
 import os
-import glob
+import argparse
 
 def main():
-    path = os.path.join(os.getcwd(), 'book-sample', 'books', '*.json.bz2')
-    volPaths = glob.glob(path)
-    fr = FeatureReader(volPaths)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='+')
+    args = parser.parse_args()
+    fr = FeatureReader(args.path)
     for vol in fr:
         all_terms = [[t] * f for t,f in vol.term_volume_freqs(case=True).iteritems()]
-        print "%s\t%s" %  (vol.id, " ".join(reduce(lambda x,y: x+y, all_terms)[1:10]) )
+        print "%s\t%s" %  (vol.id, " ".join(reduce(lambda x,y: x+y, all_terms)) )
 
 if __name__=='__main__':
     main()
