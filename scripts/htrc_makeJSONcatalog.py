2935384b72b05a12ad33cb62c9e5b50d754419aa
Peter Organisciak <organisciak@gmail.com>
2015-01-19 22:27:02 -0600

    Field description for HTRC

---
+++ scripts/htrc_makeJSONcatalog.py
@@ -0,0 +1,31 @@
+'''
+Output
+'''
+
+from htrc_features import FeatureReader
+import csv
+import os
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', nargs='+')
+    args = parser.parse_args()
+    fr = FeatureReader(args.path)
+    for vol in fr:
+        json = {}
+        for attr in ['genre', 'title', 'year', 'oclc', 'language', 'pageCount', 'imprint' ]:
+            if hasattr(vol, attr):
+                json[attr] = getattr(vol, attr)
+            else:
+                json[attr] = [] if attr == 'genre' else ''
+
+        if hasattr(vol, "handleUrl"):
+            json['searchstring'] = "<a href=\"{}\">{}</a>".format(vol.handleUrl, vol.title)
+        else:
+            json['searchstring'] = vol.title
+        print json
+        
+
+if __name__=='__main__':
+    main()
