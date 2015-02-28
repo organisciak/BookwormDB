4ce80f10168e0e278f2e761ec9e8dc0d959ac82d
Peter Organisciak <organisciak@gmail.com>
2015-02-28 14:22:15 -0600

    HTRC-specific script updates

--- scripts/htrc_featurecount_stream.py
+++ scripts/htrc_featurecount_stream.py
@@ -6,6 +6,7 @@ from htrc_features import FeatureReader
 import csv
 import os
 import argparse
+import logging
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,8 +14,15 @@ def main():
     args = parser.parse_args()
     fr = FeatureReader(args.path)
     for vol in fr:
-        for term, count in vol.term_volume_freqs(case=True).iteritems():
-            print "%s\t%s\t%i" % (vol.id.encode('utf-8'), term.encode('utf-8'), count)
+        if not vol:
+            print "Empty file"
+            continue
+        try:
+            for term, count in vol.term_volume_freqs(case=True).iteritems():
+                print "%s\t%s\t%i" % (vol.id.encode('utf-8'), term.encode('utf-8'), count)
+        except Exception, e:
+            logging.exception(e)
+
 
 if __name__=='__main__':
     main()
--- scripts/htrc_makeJSONcatalog.py
+++ scripts/htrc_makeJSONcatalog.py
@@ -6,6 +6,8 @@ from htrc_features import FeatureReader
 import csv
 import os
 import argparse
+import json
+import logging
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,18 +15,27 @@ def main():
     args = parser.parse_args()
     fr = FeatureReader(args.path)
     for vol in fr:
-        json = {}
-        for attr in ['genre', 'title', 'year', 'oclc', 'language', 'pageCount', 'imprint' ]:
-            if hasattr(vol, attr):
-                json[attr] = getattr(vol, attr)
+        if not vol:
+            logging.error("Empty file")
+            continue
+        try:
+            jmeta = {}
+            for attr in ['genre', 'title', 'year', 'oclc', 'language', 'pageCount' ]:
+                if hasattr(vol, attr):
+                    if attr == 'year':
+                        jmeta['date'] = vol.year
+                    else:
+                        jmeta[attr] = getattr(vol, attr)
+                else:
+                    jmeta[attr] = [] if attr == 'genre' else ''
+            jmeta['filename'] = vol.id
+            if hasattr(vol, "handleUrl"):
+                jmeta['searchstring'] = u"<a href=\"{0}\">{1}</a>".format(vol.handleUrl, vol.title)
             else:
-                json[attr] = [] if attr == 'genre' else ''
-        json['filename'] = vol.id
-        if hasattr(vol, "handleUrl"):
-            json['searchstring'] = "<a href=\"{}\">{}</a>".format(vol.handleUrl, vol.title)
-        else:
-            json['searchstring'] = vol.title
-        print json
+                jmeta['searchstring'] = vol.title
+            print json.dumps(jmeta)
+        except Exception, e:
+            logging.exception(e)
 
 if __name__=='__main__':
     main()
